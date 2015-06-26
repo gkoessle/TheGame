0fec9fef42c02ec9dafbefacc5af79e84b261990
Hribse <Hribse@Hribse-PC>
2015-06-26 17:53:58 +0200

    test

--- src/com/firsttry/android/AndroidLauncher.java
+++ src/com/firsttry/android/AndroidLauncher.java
@@ -12,5 +12,18 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new FirstTryMainClass(), config);
+		
+		// comment 
+		
+		
+		
+		
+		
+		// fooo
+		
+		
+		
+		
+		// test
 	}
 }
