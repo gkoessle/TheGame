9c02c2167732c4a4d2b9c3c4a2c84bd4f151a36d
Hribse <Hribse@Hribse-PC>
2015-06-26 17:42:11 +0200

    first commit

---
+++ src/com/firsttry/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.firsttry.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.firsttry.FirstTryMainClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FirstTryMainClass(), config);
+	}
+}
